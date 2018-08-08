0c082f8f4b6dd57462af0ef6acb62f75663ad40f
Vlad-Frolov <vladislavfrolov2011@gmail.com>
2018-08-08 15:21:31 +0300

    Updated navigation bar: Added: responsive dir inside the stylesheets for mobile

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :users, :controllers => {:registrations => "registrations"}
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'pages#index'
-  get 'login', to: 'devise/sessions#new'
-  get 'signup', to: 'devise/registrations#new'
+  
+  devise_scope :user do
+    get 'login', to: 'devise/sessions#new'
+    get 'signup', to: 'devise/registrations#new'
+
+  end
+  devise_for :users, :controllers => {:registrations => "registrations"}
 end
