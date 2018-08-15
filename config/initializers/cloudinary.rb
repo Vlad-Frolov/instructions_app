47a3330a404c1ff7feb8e7c662d4f920b478c512
Vlad-Frolov <vladislavfrolov2011@gmail.com>
2018-08-15 17:54:07 +0300

    Added cloudinary form

---
+++ config/initializers/cloudinary.rb
@@ -0,0 +1,7 @@
+Cloudinary.config do |config|
+    config.cloud_name = 'druq4napx'
+    config.api_key = '828254239748185'
+    config.api_secret = 'rvA8rdfEBgpT0TvQw7ELCXwyiPQ'
+    config.secure = true
+    config.cdn_subdomain = true
+  end
