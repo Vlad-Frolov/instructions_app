d8093e8959a96b4ea96d539114a1587cfb95af47
Vlad-Frolov <vladislavfrolov2011@gmail.com>
2018-08-21 16:13:01 +0300

    Added restritions to comment system

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -13,6 +13,7 @@ class Ability
       can [:create], Post
       # can :index, User
       # can :edit, User
+      can :create, Comment
       can :update, User do |u|
         u == user
       end
