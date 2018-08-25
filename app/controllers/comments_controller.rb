e4cae2a615559d178ae2b590e683ab267bd66506
Vlad-Frolov <vladislavfrolov2011@gmail.com>
2018-08-25 14:29:32 +0300

    Added user links users

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,10 +2,18 @@ class CommentsController < ApplicationController
     def upvote 
         @comment = Comment.find(params[:id])
         @comment.upvote_by current_user
+        respond_to do |format|
+          format.html { redirect_to :back }
+          format.js
+        end 
       end  
       
       def downvote
         @comment = Comment.find(params[:id])
         @comment.downvote_by current_user
+        respond_to do |format|
+          format.html { redirect_to :back }
+          format.js
+        end 
       end
 end
