54158b3d8b663dede970f8a4633c1cd0b5ee0a64
Vlad-Frolov <vladislavfrolov2011@gmail.com>
2018-09-01 17:24:22 +0300

    Views code review

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,5 @@
 class PagesController < ApplicationController
-    def index
-      # @latest = Post.order('updated_at desc').limit(5)
-    #   @posts = Post.left_outer_joins("rating_caches ON rating_caches.cacheable_id = posts.id")
-        @posts = Post.all
-    end
+  def index
+    @posts = Post.all
+  end
 end
